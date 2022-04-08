316fb7668a2bd859a01fd90c3de2cfa2b2ce4da0
Nickahlis Kopel <nickahliskopel.student@careerdevs.com>
2022-04-07 20:23:35 -0400

    initial commit

--- src/main/java/com/careerdevs/gorestsql/TestController.java
+++ src/main/java/com/careerdevs/gorestsql/TestController.java
@@ -1,4 +1,12 @@
 package com.careerdevs.gorestsql;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestController {
+
+    @GetMapping("/")
+    public String getRootRoute(){
+        return "You are at the root route";
+
+    }
 }
